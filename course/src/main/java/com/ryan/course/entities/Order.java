16db3b67d7a2bd6409600b7776872eb5c6205bf3
Ryan Silva <ryan.silva@showtecnologia.com>
2024-08-28 15:04:06 -0300

    Updated OrderItem

--- course/src/main/java/com/ryan/course/entities/Order.java
+++ course/src/main/java/com/ryan/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.ryan.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ryan.course.entities.enums.OrderStatus;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -37,7 +40,11 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> itens = new HashSet<>();
 
+	
 	public Order() {
 		super();
 	}
@@ -86,6 +93,10 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Set<OrderItem> getItems(){
+		return itens;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
