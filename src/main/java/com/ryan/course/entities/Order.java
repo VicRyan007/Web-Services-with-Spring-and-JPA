8c5a2720dd554e2d805dcf577d13a4d819ea6933
Ryan Silva <ryan.silva@showtecnologia.com>
2024-09-03 15:39:26 -0300

    insert user method added

--- src/main/java/com/ryan/course/entities/Order.java
+++ src/main/java/com/ryan/course/entities/Order.java
@@ -2,17 +2,22 @@ package com.ryan.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ryan.course.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -37,7 +42,13 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> itens = new HashSet<>();
 
+	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+	private Payment payment;
+	
 	public Order() {
 		super();
 	}
@@ -85,7 +96,27 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
 
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems(){
+		return itens;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : itens) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
